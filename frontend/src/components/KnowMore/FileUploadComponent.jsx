a4700cfca78168ab0af3caea84761891e2fb17ad
Shrinath Asati <shrinathasati111@gmail.com>
2024-02-04 06:46:32 +0530

    Update FileUploadComponent.jsx

--- frontend/src/components/KnowMore/FileUploadComponent.jsx
+++ frontend/src/components/KnowMore/FileUploadComponent.jsx
@@ -14,18 +14,14 @@ const FileUploadComponent = ({ onFileUpload }) => {
 
             const formData = new FormData();
             formData.append('resume', file);
-            console.log(formData['resume']);
-            // Assuming you have an endpoint for file upload in your Flask server
+
             const response = await axios.post('http://127.0.0.1:5000/upload_resume', formData);
 
-            // Handle the response, trigger scanning, etc.
             console.log(response.data);
 
-            // Reset the file state after successful upload
             setFile(null);
 
-            // Pass the uploaded file information to the parent component
-            onFileUpload(response.data);
+            onFileUpload(response.data);  // You can handle the response data as needed in your React component
         } catch (error) {
             console.error('Error uploading file:', error);
         }
@@ -38,26 +34,28 @@ const FileUploadComponent = ({ onFileUpload }) => {
 
                 <div className='px-2 text-sm '>Sccan</div>
             </div>
-            <div className='flex m-2  p-4 cursor-pointer bg-white border border-gray-300 rounded-lg shadow-md'>
+        <div className='flex m-2  p-4 cursor-pointer bg-white border border-gray-300 rounded-lg shadow-md'>
+            
+            <label htmlFor='fileInput' className='w-[100%] cursor-pointer'>
+                <input
+                    id='fileInput'
+                    type='file'
+                    // className='hidden'
+                    onChange={(e) => setFile(e.target.files[0])}
+                />
+                <FaUpload className='text-green-500 text-2xl' />
+                <p className='ml-2 text-gray-600'>Choose a file</p>
+            </label>
 
-                <label htmlFor='fileInput' className='w-[100%] cursor-pointer'>
-                    <input
-                        id='fileInput'
-                        type='file'
-                        onChange={(e) => setFile(e.target.files[0])}
-                    />
-                    <FaUpload className='text-green-500 text-2xl' />
-                    <p className='ml-2 text-gray-600'>Choose a file</p>
-                </label>
-
-                <div className='flex justify-center'>
-                    <button onClick={handleFileUpload} className='bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600'>
-                        Upload
-                    </button>
-                </div>
+            <div className='flex justify-center'>
+                <button onClick={handleFileUpload} className='bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600'>
+                    Upload
+                </button>
+            </div>
             </div>
         </>
     );
 };
 
 export default FileUploadComponent;
+
